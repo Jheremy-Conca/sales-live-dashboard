23b362e735057c1e5bb11ae04ab18d3a5ef91db7
Jheremy-Conca <concajheremy@gmail.com>
2026-07-16 15:42:09 -0500

    Fix socket.js duplicado + config Netlify

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL;
 
-export const socket = io(URL, {
+export const socket = io(API_URL, {
   autoConnect: true,
 });
